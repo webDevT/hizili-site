54b5ae21812374fc7177866572aed36f643d9cf1
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-09-11 12:42:13 +0300

    user page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
-
+	$(".profile__tab").click(function() {
+	$(".profile__tab").removeClass("active").eq($(this).index()).addClass("active");
+ 	$(".profile__tab-item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
 
 });
