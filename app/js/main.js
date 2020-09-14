3574b64747909842707465523ce53acb90cb3e5f
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-09-14 13:06:54 +0300

    new pages

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,14 @@ $(function(){
  	$(".profile__tab-item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
+
+$('.hesap-ac__item').click(function(){	
+		$(this).toggleClass('active');	
+		$('.hesap-ac__button').removeClass('disabled');
+
+	});
+
+
+
+
 });
